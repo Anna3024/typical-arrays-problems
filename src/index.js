f36b0a7e3d1dace17fa94be994d3c37649908d1b
Anna3024 <kurban.anna@ab-ds.ru>
2021-03-15 23:43:50 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array==undefined||array == null || array.length==0) return 0;
+  let myArr = [];
+  for (let i=0; i<array.length; i++) {
+    myArr.push(array[i]);
+  }
+  let min = myArr.reduce((r,v)=>Math.min(r,v), 100000)
+  return  min;
+  // return array.reduce((r,v)=>Math.min(r,v), Number.POSITIVE_INFINITY);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array==undefined||array == null || array.length==0) return 0;
+  let myArr = [];
+  for (let i=0; i<array.length; i++) {
+    myArr.push(array[i]);
+  }
+  let max = myArr.reduce((r,v)=>Math.max(r,v), -100000)
+  return max;
+  // return array.reduce((r,v)=>Math.max(r,v), Number.NEGATIVE_INFINITY);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array==undefined||array == null || array.length==0) return 0;
+  let myArr = [];
+  let summ = 0;
+  for (let i=0; i<array.length; i++) {
+    myArr.push(array[i]);
+    summ+=array[i];
+  }
+  let res = summ/(myArr.length)
+  return res;
 }
